5d20b8ccce2ef44433ab6df0257ea155d90abaf0
Robert McCary <robert.tyler.mccary@gmail.com>
2019-07-20 14:58:58 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,25 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+
+  def create 
+    @game = Game.create(game_params)
+  end 
+
+  def show 
+
+  end 
+
+  def update 
+
+  end 
+
+  def index 
+    @games = Game.all 
+  end 
+
+  private 
+
+  def game_params 
+    params.require(:game).permit(:state)
+  end 
 
 end
